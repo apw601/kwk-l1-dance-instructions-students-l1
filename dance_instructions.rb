cc1f90d5480acf657a8a37195b866949dc1e34bf
apw601 <apw601@gmail.com>
2018-06-04 23:21:52 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,1 +1,13 @@
-def Ganghnam 
+def Gangnam_Style
+  puts "Lower Body"
+  puts "Assume the proper stance"
+  puts "1. Plant legs far apart, bend knees slightly and keep posture loose"
+  puts "Base Footwork"
+  puts "1. Lift right foot"
+  puts "2. Return right foot to the ground"
+  puts "3. Finishing with a small skip-stop backward"
+  
+  puts "Skip Step"
+  puts "Skip Step Left"
+  puts "1. Lower left foot to the ground"
+  "
