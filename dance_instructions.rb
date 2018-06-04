0549aa5c06793cce65f5f7d18a5263eeeb6deece
apw601 <apw601@gmail.com>
2018-06-04 23:22:25 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -10,4 +10,20 @@ def Gangnam_Style
   puts "Skip Step"
   puts "Skip Step Left"
   puts "1. Lower left foot to the ground"
-  "
+  Bounce left foot back up slightly, kicking it a few inches back
+Left, Right, Left and Left
+Skip Step Right
+
+Lower right foot to the ground
+Bounce right foot back up slightly, kicking it a few inches back
+Right, Left, Right and Right
+UPPER BODY
+BOB THE REINS
+Holding your arms out in front of you, straight and at chest level
+Cross your right wrist over your left and hold them together
+Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. This movement is repeated eight times.
+Lasso
+
+Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left.
+Lift right arm up and point right elbow diagonally to the right.
+Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well.
